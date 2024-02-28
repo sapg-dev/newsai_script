2319a7b46d8db778b203ed30e2b8da3c4b0b1e8e
Sacha <40305546+sapg-dev@users.noreply.github.com>
2024-02-28 20:33:12 +0100

    csv to mongo migration
    added the exported mongo db data in json format, added sitemap.csv for csv to mongo test. added pymongo to requirements.txt

---
+++ csv_to_mongo.py
@@ -0,0 +1,34 @@
+import csv
+from pymongo import MongoClient
+import urllib.parse
+
+
+username = urllib.parse.quote_plus('blue-sitemaps') #has to be removed for production
+password = urllib.parse.quote_plus('h72f4X9OnN396p2P') #has to be removed for production
+cluster = 'blue.hgntyje.mongodb.net' #has to be removed for production
+
+
+csv_file_path = 'data/sitemap.csv'
+
+
+connection_string = f'mongodb+srv://{username}:{password}@{cluster}/?retryWrites=true&w=majority&tlsAllowInvalidCertificates=true'
+client = MongoClient(connection_string)
+
+db = client['blue-sitemaps']
+collection = db['data']
+
+# Read CSV and insert into MongoDB
+def csv_to_mongodb(csv_file_path):
+    try:
+        with open(csv_file_path, mode='r', encoding='utf-8') as file:
+            csv_reader = csv.DictReader(file)
+
+            for row in csv_reader:
+                print(row)  # Print the current row
+                collection.insert_one(row)
+        print("Data insertion completed.")
+    except Exception as e:
+        print(f"An error occurred: {e}")
+
+# Execute the insertion
+csv_to_mongodb(csv_file_path)
